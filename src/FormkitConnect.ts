e14cd27cd108c464621db51fa3e27e97663f224b
ivan <ipkozyrin@gmail.com>
2018-06-21 13:30:47 +1000

    change

--- src/FormkitConnect.ts
+++ src/FormkitConnect.ts
@@ -3,6 +3,7 @@ import * as _ from 'lodash';
 import formkit from 'formkit';
 
 import helpers from './helpers';
+import FormInterface from './FormInterface';
 
 
 interface Props {
@@ -15,9 +16,13 @@ interface State {
   wasStateInited: boolean;
 }
 
+
 export default function formkitConnect(config) {
   return function decorator(Target) {
     return class Wrapper extends React.PureComponent<Props, State> {
+      // TODO: use form class from formkit
+      private form: any;
+
       static contextTypes = Target.contextTypes;
 
       state = {
@@ -26,10 +31,10 @@ export default function formkitConnect(config) {
         wasStateInited: false,
       };
 
-      componentWillReceiveProps(nextProps) {
+      componentWillReceiveProps(nextProps: Props) {
         if (_.isEqual(nextProps.initialValues, this.props.initialValues)) return;
 
-        this._updateSavedValues(nextProps);
+        this.updateSavedValues(nextProps);
       }
 
       componentWillMount() {
@@ -41,31 +46,32 @@ export default function formkitConnect(config) {
           throw new Error(`validate callback has to be a function!`);
 
         // get instance of form
-        this._form = this._instantiateForm(config);
+        this.form = this.instantiateForm(config);
         // init form with specified fields and initial values
-        this._initForm();
+        this.initForm();
         // update react state on each change
-        this._form.on('storage', this.handleStorageChange);
+        this.form.on('storage', this.handleStorageChange);
       }
 
       componentWillUnmount() {
-        this._form.destroy();
+        this.form.destroy();
       }
 
       handleStorageChange = (data) => {
 
         // TODO: review
 
-        //this._updateField(data);
-        this._updateFields();
-        this.setState({ formState: helpers.makeFormState(this._form) });
+        //this.updateField(data);
+        this.updateFields();
+        this.setState({ formState: helpers.makeFormState(this.form) });
       };
 
       getWrappedInstance() {
         return this.refs.instance;
       }
 
-      _instantiateForm(connectorConfig) {
+
+      private instantiateForm(connectorConfig) {
         if (connectorConfig.getForm) {
           return config.getForm(this.props, this.context);
         }
@@ -73,20 +79,20 @@ export default function formkitConnect(config) {
         return formkit.newForm(connectorConfig.config);
       }
 
-      _initForm() {
+      private initForm() {
         // validate wrapper needs to passing props to validate callback
         const validateWrapper = (errors, values) => config.validate(errors, values, this.props);
         const fieldsInitial = helpers.generateFieldsInitParams(config.fields, this.props.initialValues);
 
-        this._form.init(fieldsInitial, config.validate && validateWrapper);
+        this.form.init(fieldsInitial, config.validate && validateWrapper);
 
         // set state of form and fields
-        this._initState();
+        this.initState();
       }
 
-      _initState() {
-        const formState = helpers.makeFormState(this._form);
-        const fields = helpers.fillFieldsState(this._form.fields);
+      private initState() {
+        const formState = helpers.makeFormState(this.form);
+        const fields = helpers.fillFieldsState(this.form.fields);
 
         this.setState({ formState, fields }, () => {
           // it needs for componentWillMount of underlying component receives form and field state in props
@@ -94,7 +100,7 @@ export default function formkitConnect(config) {
         });
       }
 
-      _updateSavedValues(props) {
+      private updateSavedValues(props) {
 
         // TODO: review
 
@@ -104,16 +110,16 @@ export default function formkitConnect(config) {
         }
 
         // set initial values
-        this._form.setSavedValues(initialValues);
+        this.form.setSavedValues(initialValues);
       }
 
       // TODO: из за этого получается рассогласование полей если одно поле меняет другое
-      _updateField(eventData) {
+      private updateField(eventData) {
         if (eventData.target !== 'field') return;
 
         const fields = _.clone(this.state.fields);
         const currentState = _.get(fields, eventData.field);
-        const field = _.get(this._form.fields, eventData.field);
+        const field = _.get(this.form.fields, eventData.field);
         const updatedField = _.defaultsDeep(helpers.makeFieldState(field), currentState);
 
         _.set(fields, eventData.field, updatedField);
@@ -121,7 +127,7 @@ export default function formkitConnect(config) {
         this.setState({ fields });
       }
 
-      _updateFields() {
+      private updateFields() {
         const fields = _.clone(this.state.fields);
 
         const recursively = (container, path) => {
@@ -146,15 +152,15 @@ export default function formkitConnect(config) {
 
         };
 
-        recursively(this._form.fields, '');
+        recursively(this.form.fields, '');
 
         this.setState({ fields });
       }
 
-      _handleSubmit = (event) => {
+      private handleSubmit = (event) => {
         event.preventDefault();
         event.stopPropagation();
-        this._form.handleSubmit();
+        this.form.handleSubmit();
       };
 
 
@@ -164,7 +170,7 @@ export default function formkitConnect(config) {
           return React.createElement(Target, {
             ref: 'instance',
             ...this.props,
-            form: this._form,
+            form: this.form,
             fields: this.state.fields,
             values: this.state.formState.values,
             savedValues: this.state.formState.savedValues,
@@ -177,7 +183,7 @@ export default function formkitConnect(config) {
             submittable: this.state.formState.submittable,
             valid: this.state.formState.valid,
             errors: this.state.formState.errors,
-            handleSubmit: this._handleSubmit,
+            handleSubmit: this.handleSubmit,
           });
         }
         else {
